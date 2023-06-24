5539aca76821baafe12275e00ca7ba4c528e3ad0
Reuben Reyes <reuben.a.reyes@gmail.com>
2023-06-24 16:54:32 -0700

    feat(state): #1 - use protobuf as user schema protocol

--- go.mod
+++ go.mod
@@ -2,12 +2,4 @@ module github.com/rreubenreyes/orchid
 
 go 1.20
 
-require (
-	github.com/golang/snappy v0.0.4 // indirect
-	github.com/hamba/avro/v2 v2.12.0 // indirect
-	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/linkedin/goavro/v2 v2.12.0 // indirect
-	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.2 // indirect
-)
+require google.golang.org/protobuf v1.30.0
--- internal/state/state.go
+++ internal/state/state.go
@@ -1,49 +1,113 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 
-	"github.com/linkedin/goavro/v2"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
 type State struct {
-	schema  *goavro.Codec
-	history []map[string]any // keep state history in memory for now, will move to db or something later
-	value   map[string]any
+	messageType protoreflect.MessageType
+	value       protoreflect.Message
 }
 
-func (s State) Schema() *goavro.Codec {
-	return s.schema
+func (s *State) Init(mt protoreflect.MessageType) {
+	if s.messageType != nil {
+		fmt.Printf("state already initialized with message type")
+	}
+
+	s.messageType = mt
 }
 
-func (s State) Value() map[string]any {
+func (s State) MessageDescriptor() protoreflect.MessageDescriptor {
+	return s.messageType.Descriptor()
+}
+
+func (s State) Value() protoreflect.Message {
 	return s.value
 }
 
-func (s *State) LoadSchema(data []byte) error {
-	codec, err := goavro.NewCodec(string(data))
-	if err != nil {
-		return err
+func (s State) ValueAtPath(path string) (any, error) {
+	pathArr := strings.Split(path, ".")
+
+	var curPath string
+	var travelledPath []string
+	var fd protoreflect.FieldDescriptor
+	var cur any
+	var next any
+
+	cur = s.value
+	for len(pathArr) > 0 {
+		curPath = pathArr[0]
+		travelledPath = append(travelledPath, pathArr[0])
+		pathArr = pathArr[1:]
+
+		switch cur := cur.(type) {
+		case protoreflect.Message:
+			fds := cur.Descriptor().Fields()
+
+			if !cur.Has(fd) {
+				return nil, fmt.Errorf("path %s not present in state", strings.Join(travelledPath, "."))
+			}
+
+			fd := fds.ByJSONName(curPath)
+			if fd.Kind() == protoreflect.BoolKind {
+				next = cur.Get(fd).Bool()
+			}
+			if fd.Kind() == protoreflect.EnumKind {
+				next = cur.Get(fd).Enum()
+			}
+			if fd.Kind() == protoreflect.Int32Kind ||
+				fd.Kind() == protoreflect.Int64Kind ||
+				fd.Kind() == protoreflect.Sint32Kind ||
+				fd.Kind() == protoreflect.Sint64Kind ||
+				fd.Kind() == protoreflect.Sfixed32Kind ||
+				fd.Kind() == protoreflect.Sfixed64Kind {
+				next = cur.Get(fd).Int()
+			}
+			if fd.Kind() == protoreflect.Uint32Kind ||
+				fd.Kind() == protoreflect.Uint64Kind ||
+				fd.Kind() == protoreflect.Fixed32Kind ||
+				fd.Kind() == protoreflect.Fixed64Kind {
+				next = cur.Get(fd).Uint()
+			}
+			if fd.Kind() == protoreflect.FloatKind ||
+				fd.Kind() == protoreflect.DoubleKind {
+				next = cur.Get(fd).Float()
+			}
+			if fd.Kind() == protoreflect.StringKind {
+				next = cur.Get(fd).String()
+			}
+			if fd.Kind() == protoreflect.BytesKind {
+				next = cur.Get(fd).Bytes()
+			}
+			if fd.Kind() == protoreflect.MessageKind ||
+				fd.Kind() == protoreflect.GroupKind {
+				next = cur.Get(fd).Message()
+			}
+
+			next = cur.Get(fd)
+		}
+
+		cur = next
 	}
 
-	s.schema = codec
-
-	return nil
+	return nil, nil
 }
 
-func (s *State) Update(data []byte) (map[string]any, error) {
-	native, _, err := s.schema.NativeFromTextual(data)
-	if err != nil {
-		return nil, err
+func (s *State) Update(data []byte) (protoreflect.Message, error) {
+	if s.messageType == nil {
+		return nil, errors.New("state not initialized")
 	}
 
-	switch native := native.(type) {
-	case map[string]any:
-		s.value = native
-		s.history = append(s.history, native)
-
-		return s.value, nil
-	default:
-		return nil, fmt.Errorf("data unmarshaled into unexpected type")
+	m := s.messageType.New().Interface()
+	err := protojson.Unmarshal(data, m)
+	if err != nil || !m.ProtoReflect().IsValid() {
+		return nil, errors.New("data does not match message format")
 	}
+
+	return m.ProtoReflect(), nil
 }
